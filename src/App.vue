81cda0477cfb938839f3b816b48435ec1d9de21a
TheMrStraffy <carmelo.miuccio2@gmail.com>
2022-11-14 15:35:03 +0100

    [vue]

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+
+  export default {
+    components: { AppTitle },
+    name: "App",
+    AppTitle
+  }
+</script>
+
+<template>
+
+  <AppTitle/>
+
+</template>
+
+
+<style>
+
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,29 @@
+<script>
+  export default {
+    name: "AppTitle",
+    data(){
+      return{
+        title: "La mia prima app con Vite!"
+      }
+    }
+  }
+</script>
+<template>
+  <div>
+
+    <h1>{{title}}</h1>
+  </div>
+</template>
+
+
+<style scoped>
+div{
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+h1{
+  background-color: aqua;
+}
+</style>
